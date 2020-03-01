54d5900655ade1d86b3625e2a14d0027e2164492
monpdev <35469743+monpdev@users.noreply.github.com>
2020-03-01 03:02:02 +0000

    Add files via upload

---
+++ secretMessage.js
@@ -0,0 +1,20 @@
+//secretMessage.
+//Transform an array of strings into a secret message
+//Part of Codecademy/Web Dev Track
+
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+console.log(secretMessage.length);
+secretMessage.pop();
+console.log(secretMessage.length);
+
+secretMessage.push('to','Program');
+
+secretMessage[7] = 'right';
+secretMessage.shift();
+secretMessage.unshift('Programming');
+
+secretMessage.splice(6,5,'know,');
+
+console.log(secretMessage.join(' '));
+
