df9bace2637f37f5031c014188b73271f93e017f
monpdev <35469743+monpdev@users.noreply.github.com>
2020-09-07 11:11:04 +0100

    Add files via upload

---
+++ SQL/sqlChallenge.sql
@@ -0,0 +1,44 @@
+SELECT year, number
+FROM babies
+WHERE name = 'Lillian' AND gender = 'F';
+
+SELECT DISTINCT name
+FROM babies
+WHERE name LIKE 'S%'
+LIMIT 20;
+
+
+SELECT name, gender, number
+FROM babies
+WHERE year = 1880
+ORDER BY number DESC
+LIMIT 10;
+
+
+SELECT *
+FROM nomnom
+WHERE cuisine = 'Japanese'
+  AND price = '$$';
+
+
+SELECT *
+FROM nomnom
+WHERE name LIKE '%noodle%';
+
+SELECT *
+FROM nomnom
+WHERE health IS NULL;
+
+SELECT title,publisher
+FROM news
+ORDER BY title;
+
+SELECT *
+FROM news
+WHERE title LIKE '%bitcoin%'; 
+
+ SELECT *
+FROM news
+WHERE category = 'b'
+ORDER BY timestamp DESC
+LIMIT 20; 
