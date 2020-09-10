399e2fef741d989f27523f22c3eccf49f2c806b6
monpdev <35469743+monpdev@users.noreply.github.com>
2020-09-10 10:50:11 +0100

    Add files via upload

---
+++ SQL/creatingUpdatingDeleting.sql
@@ -0,0 +1,45 @@
+--SQL exercise to learn about creating, updating and deleting data
+
+SELECT * FROM celebs;
+
+CREATE TABLE celebs(
+  id INTEGER,
+  name TEXT,
+  age INTEGER
+);
+
+INSERT INTO celebs (id, name, age)
+VALUES (1, 'Justin Bieber',22);
+
+INSERT INTO celebs (id, name, age) 
+VALUES (2, 'Beyonce Knowles', 33); 
+
+INSERT INTO celebs (id, name, age) 
+VALUES (3, 'Jeremy Lin', 26); 
+
+INSERT INTO celebs (id, name, age) 
+VALUES (4, 'Taylor Swift', 26);
+
+
+ ALTER TABLE celebs
+ ADD COLUMN twitter_handle TEXT;
+
+ SELECT * FROM celebs;
+
+UPDATE celebs
+SET twitter_handle = '@taylorswift13'
+WHERE id = 4;
+
+SELECT * FROM celebs;
+
+
+ DELETE FROM celebs
+ WHERE twitter_handle IS NULL;
+ SELECT * FROM celebs;
+
+
+CREATE TABLE awards(
+id INTEGER PRIMARY KEY,
+recipient TEXT NOT NULL,
+award_name TEXT DEFAULT 'Grammy'
+ );
