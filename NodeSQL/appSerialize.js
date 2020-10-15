893b72bb5b5109d9a83935faabfcdd8caed26078
monpdev <35469743+monpdev@users.noreply.github.com>
2020-10-15 23:33:55 +0100

    Add files via upload

---
+++ NodeSQL/appSerialize.js
@@ -0,0 +1,9 @@
+const db = require('./db');
+
+db.serialize(() => {                                                                                                                          
+db.run('CREATE TABLE Popcorn (id INTEGER PRIMARY KEY, type TEXT)');
+db.run('INSERT INTO POPCORN (type) VALUES ("cheddar")');
+db.run('INSERT INTO POPCORN (type) VALUES ("kettle corn")');
+
+});
+
